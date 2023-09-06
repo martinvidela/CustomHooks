a0852f481a8ee137d2c89585c3b8ce9aa79c8bc9
martinvidela <videla116@gmail.com>
2023-09-06 12:59:04 -0300

    'mishooks'

---
+++ useTodo.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useReducer } from "react";
+import { todoReducer } from "../08-useReducer/todoReducer";
+import { TodoList } from "../Components/TodoList";
+
+const init = () => {
+  return JSON.parse(localStorage.getItem("todos")) || [];
+};
+
+export const useTodo = (initialState = []) => {
+  const [todos, dispatch] = useReducer(todoReducer, initialState, init);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const handleNewTodo = (todo) => {
+    const action = {
+      type: "Add Todo",
+      payload: todo,
+    };
+
+    dispatch(action);
+  };
+
+  const deleteTodo = (id) => {
+    const action = {
+      type: "Remove Todo",
+      payload: id,
+    };
+    dispatch(action);
+  };
+
+  const handleToggleTodo = (id) => {
+    const action = {
+      type: "Toggle Todo",
+      payload: id,
+    };
+    dispatch(action);
+  };
+
+  return {
+    handleNewTodo,
+    deleteTodo,
+    handleToggleTodo,
+    todos,
+    todosCount: todos.length,
+    PendingTodos: todos.filter(todo=> !todo.done).lenght
+  };
+};
